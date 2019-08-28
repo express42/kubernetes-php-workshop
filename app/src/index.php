829f7d3d11cef1b39c491d86fae561a488a9ec27
Nikita Velgin <direct.vegas@gmail.com>
2019-08-28 20:12:28 +0300

    Add load script to php. Move metrics to /metrics path

--- app/src/index.php
+++ app/src/index.php
@@ -1,21 +1,11 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
-use Prometheus\CollectorRegistry;
-use Prometheus\RenderTextFormat;
-use Prometheus\Storage\InMemory;
 
-include( './counter.php' ); 
+include( './counter.php' );
+ 
+$x = 0.0001;
+for ($i = 0; $i <= 1000000; $i++) {
+  $x += sqrt($x);
+}
+echo "OK!";
 
-$adapter = 'in-memory';
-$adapter = new Prometheus\Storage\InMemory();
-
-$registry = new CollectorRegistry($adapter);
-$counter = $registry->registerCounter('hits', 'counter', 'it increases', ['type']);
-$counter->incBy($count, ['hits']);
-
-$registry = new CollectorRegistry($adapter);
-$renderer = new RenderTextFormat();
-$result = $renderer->render($registry->getMetricFamilySamples());
-header('Content-type: ' . RenderTextFormat::MIME_TYPE);
-echo $result
 ?>
